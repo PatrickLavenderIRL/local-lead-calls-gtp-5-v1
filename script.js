61240aacd01bde8a2b1ff142881e7f89e893a1c7
PatrickLavenderIRL <patricklavender01@gmail.com>
2025-08-07 19:57:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch('assets/data.json')
-    .then(response => response.json())
-    .then(data => {
-        const output = document.getElementById('data-output');
-        output.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
-    })
-    .catch(err => console.error("Error loading data:", err));
+  .then(res => res.json())
+  .then(data => {
+    const output = document.getElementById('data-output');
+    output.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+  })
+  .catch(err => console.error("Error:", err));
