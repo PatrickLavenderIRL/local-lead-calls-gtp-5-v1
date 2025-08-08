6e6053433baaf05b470963a4bbef5a5cfb9814a2
PatrickLavenderIRL <patricklavender01@gmail.com>
2025-08-07 20:37:55 -0400

    Update form-handler.js

--- assets/js/form-handler.js
+++ assets/js/form-handler.js
@@ -1,4 +1,4 @@
 document.getElementById("contact-form").addEventListener("submit", function(e) {
   e.preventDefault();
-  alert("Your form has been submitted!");
+  alert("Thank you for your message! We will get back to you soon.");
 });
